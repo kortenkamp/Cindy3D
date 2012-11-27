79e92214a219303636c1d493419ec7f9d9ec6003
Martin von Gagern <Martin.vGagern@gmx.net>
2012-11-27 09:16:54 +0100

    Allow generating only the body of the HTML reference. This is useful for processing by Jekyll for the GitHub Pages, to better integrate this documentation with the rest of the page.

---
+++ Reference/mkPage.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+
+import docutils.core
+import docutils.io
+import os.path
+import re
+import shutil
+import sys
+
+settings_overrides={'output_encoding': 'unicode',
+                    'embed_stylesheet': False,
+                    }
+output, pub = docutils.core.publish_programmatically(
+    source_class=docutils.io.FileInput,
+    source=None,
+    source_path='CommandReference.rst',
+    destination_class=docutils.io.StringOutput,
+    destination=None,
+    destination_path=None,
+    reader=None, reader_name='standalone',
+    parser=None, parser_name='restructuredtext',
+    writer=None, writer_name='html',
+    settings=None,
+    settings_spec=None,
+    settings_overrides=settings_overrides,
+    config_section=None,
+    enable_exit_status=False,
+    )
+m = re.search(r'<body[^>]*>(.*)</body>', output,
+              re.IGNORECASE|re.DOTALL)
+body = m.group(1)
+m = re.search(r'<title[^>]*>(.*)</title>', output,
+              re.IGNORECASE|re.DOTALL)
+title = m.group(1)
+m = re.search('<link\s+rel=(["\'])stylesheet\\1\s+href=(["\'])([^"\']*)\\2',
+              output, re.IGNORECASE)
+css = m.group(3)
+cssn = os.path.join('..', 'stylesheets', os.path.basename(css))
+os.chdir(sys.argv[1])
+shutil.copyfile(css, cssn)
+with open('CommandReference.html', 'w', encoding='utf-8') as f:
+    f.write('''\
+---
+layout: main
+title: Cindy 3D {title}
+styles: {cssn}
+topdir: ../
+---
+{body}'''.format_map(locals()))
