45b48482ba9dce706144c6b627c2422667443cf5
Martin von Gagern <Martin.vGagern@gmx.net>
2014-05-15 17:23:24 +0200

    Update Jogl version in the build file.
    This ensures people get the same jogl version when building from source.

--- update-jogl.sh
+++ update-jogl.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-urlbase=http://jogamp.org/deployment/jogamp-current/jar
-files=( )
-for i in Cindy3D/lib/3rd-party/{gluegen,jogl}*.jar; do
-    [[ -e "${i}.tmp" ]] && rm "${i}.tmp"
-    wget -O "${i}.tmp" "${urlbase}/${i##*/}" || exit $?
-    files+=( "${i}" )
-done
-for i in "${files[@]}"; do
-    mv "${i}.tmp" "${i}"
-done
+set -e
+cd Cindy3D
+rm -vf lib/3rd-party/{gluegen,jogl}*.jar
+ant deps.jogl
